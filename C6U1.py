9ed28c932e78ad385a5733fa20204784c3631de4
Mag3P14 <92228922+Mag3P14@users.noreply.github.com>
2021-10-31 15:24:25 +0100

    Update C6U1.py

--- C6U1.py
+++ C6U1.py
@@ -1,6 +1,6 @@
 
-s=[["4x+3t=5q+o+j+40"],["4x+4z+2j=3q+4e+4t+2o+20"],["2t+9=5q+e+z+4o+3j"],["x+2e+2t+3z+o+4j=51"],["4q+73=5x+2e+3t+4o+3j"],["q+3z+2o+3j+19=5x+2e+t"],["2z+3j+1=3x+3q+2e+5o]"]]
-
+s=[["4x+3t=5q+o+j+40"],["4x+4z+2j=3q+4e+4t+2o+20"],["2t+9=5q+e+z+4o+3j"],["x+2e+2t+3z+o+4j=51"],["4q+73=5x+2e+3t+4o+3j"],["q+3z+2o+3j+19=5x+2e+t"],["2z+3j+1=3x+3q+2e+5o"]]
+#ošetřit proti neznámým typu "1*q"
 #s je testovací input
 pocetRovnic=len(s)
 listNeznamych = []
@@ -16,7 +16,6 @@ k = 0
 def strint(list):
     l = list
     list = [[int(i) for i in list]for list in l]
-    l.clear
     return list
 
 # rovnaSeSplit rozdělení přes znak "="
@@ -29,38 +28,47 @@ def formatovaniRovnice(s):
     for i in range(len(levaStrana)):
         x = levaStrana[i][-1]
         if x not in listNeznamych:
-            listNeznamych.append(x)
+            if x in letters:
+                listNeznamych.append(x)
 
     for i in range(len(pravaStrana)):
         x = pravaStrana[i][-1]
         if x not in listNeznamych:
-            listNeznamych.append(x)
+            if x in letters:
+                listNeznamych.append(x)
 
 for i in range(pocetRovnic):
     formatovaniRovnice(s[i][0])
-#
-
-for i in range(len(listNeznamych)-2):
+ 
+for i in range(len(listNeznamych)-1):
         if any(x in listNeznamych[i] for x in numbers):
             del listNeznamych[i]
 
 pocetNeznamych = len(listNeznamych)
 listNeznamych.sort()
 
+
 for i in range(pocetRovnic):
     j=0
-    while j<len(listPravychStran[i])-1:
+    while j<len(listPravychStran[i]):
+        if not any(x in listPravychStran[i][j] for x in numbers):
+            listPravychStran[i][j] = "1"+listPravychStran[i][j]
         if any(x in listPravychStran[i][j] for x in listNeznamych):
             listLevychStran[i].append("-"+listPravychStran[i][j])
             del listPravychStran[i][j]
         else:
             j+=1
 
-print(s)
-print("listLevychStran:",listLevychStran)
-print("listPravychStran:",listPravychStran)
-print("listNeznamych:",listNeznamych)
-print("pocetNeznamych:",pocetNeznamych)
+for i in range(pocetRovnic):
+    j=0        
+    while j<len(listLevychStran[i]):
+        if not any(x in listLevychStran[i][j] for x in numbers): #pokud mám vlevo něco jako "q" změním to na "1q"
+            listLevychStran[i][j] = "1"+listLevychStran[i][j]
+        if not any(x in listLevychStran[i][j] for x in listNeznamych):
+            listPravychStran[i].append("-"+listLevychStran[i][j])
+            del listLevychStran[i][j]
+        else:
+            j+=1
 
 #vytvořím matici o [pocetRovnic] řádcích a [pocetNeznamých] sloupcích
 
@@ -81,6 +89,14 @@ for i in range(len(listLevychStran)):
             found = False
     k+=1
 
-print(strint(matice))
-print(strint(listPravychStran))
+matice = strint(matice)
+listPravychStran = strint(listPravychStran)
+print(listPravychStran)
 
+for i in range(len(s)):
+    print(s[i])
+print("listNeznamych:",listNeznamych)
+
+print("matice")
+for i in range(len(matice)):
+    print(matice[i],listPravychStran[i])
